c0e447f1c3f903647831a6170e93a191baf630ce
Santiago So <Santiagosuarez1136374@correo.itm.edu.co>
2026-05-28 23:53:03 -0500

    Comenta método OnConfiguring en CineColombiaContext
    Se ha comentado el método OnConfiguring para evitar exponer la cadena de conexión directamente en el código fuente y mejorar la seguridad.

--- Models/CineColombiaContext.cs
+++ Models/CineColombiaContext.cs
@@ -91,10 +91,10 @@ public partial class CineColombiaContext : DbContext
 
     public virtual DbSet<Ventum> Venta { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+   /* protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=CineColombia;Trusted_Connection=True;TrustServerCertificate=True;");
-
+   */
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Boletica>(entity =>
